60fe2b62fdc25403b2a2387386df6c30d734dd5f
Leon Dick <ldick@1-B-1.42heilbronn.de>
2024-09-16 15:28:22 +0200

    created basic layout

---
+++ includes/minishell.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldick <ldick@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/19 17:36:55 by ldick             #+#    #+#             */
+/*   Updated: 2024/09/16 15:21:18 by ldick            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "libs.h"
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+
+typedef struct s_nodes
+{
+
+}			t_nodes;
+
+
+
+
+
+#endif
