92b7f9cd2a6a301044abec3caaefcf2f9ab3326d
Leon Dick <ldick@student.42heilbronn.de>
2024-12-16 09:01:04 +0100

    pulling newest branch

--- srcs/utils/parsing.c
+++ srcs/utils/parsing.c
@@ -6,7 +6,7 @@
 /*   By: ldick <ldick@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 18:53:04 by ldick             #+#    #+#             */
-/*   Updated: 2024/12/11 17:01:27 by ldick            ###   ########.fr       */
+/*   Updated: 2024/12/16 08:57:27 by ldick            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,19 @@
 void	init_history(void)
 {
 	int		fd;
-	int		i;
-	int		j;
-	char	buffer[7000];
-	char	tmp[5000];
+	char	*line;
 
-	i = 0;
-	j = 0;
-	fd= open(".git/permanent_history/history.log" , O_RDONLY);
-	read(fd, buffer, 7000);
-	if (fd != -1)
+	fd = open(".git/permanent_history/history.log" , O_RDONLY);
+	while(1)
 	{
-		while(buffer[i])
-		{
-			if (buffer[i + 1] == '\n')
-			{
-				j = 0;
-				add_history(tmp);
-			}
-			tmp[j] = buffer[i];
-			i++;
-			j++;
-		}
+		line = get_next_line(fd);
+		if (line == NULL)
+			break;
+		line[ft_strlen(line) - 1] = '\0';
+		add_history(line);
+		line = NULL;
 	}
+	close(fd);
 }
 
 void	add_permanent_history(char *str)
@@ -52,32 +42,10 @@ void	add_permanent_history(char *str)
 	close(fd);
 }
 
-char	**ft_tokenize(char *input)
-{
-	char	**token_stack;
-
-	token_stack = ft_split(input, ' ');
-	return (token_stack);
-}
-
 static char	ft_is_whitespace(char c)
 {
 	if ((c >= 9 && c <= 13) || c == 32)
 		return (1);
-	return (0);
-}
-
-static int	possible_var(char *input)
-{
-	int	i;
-
-	i = 0;
-	while (input[i])
-	{
-		if (input[i] == '=')
-			return (1);
-		i++;
-	}
 	return (0);
 }
 
@@ -93,77 +61,26 @@ static void	print_two_d(char **array)
 	}
 }
 
-static int	check_var(char *input)
-{
-	int	i;
-
-	i = 0;
-	while(input[i])
-	{
-		if (!input[i])
-			break ;
-		if (input[i] == '=' && ft_is_whitespace(input[i + 1]))
-			return (3);
-		else if (input[i + 1] == '"' || input[i + 1] == '\'')
-		{
-			i++;
-			i++;
-			while(input[i])
-			{
-				if (input[i] == '"' || input[i] == '\'')
-					return (1);
-				i++;
-			}
-		}
-		else if (input[i + 1] && !ft_is_whitespace(input[i + 1]))
-		{
-			i++;
-			i++;
-			while(input[i])
-			{
-				if (ft_is_whitespace(input[i]))
-					return (1);
-				i++;
-			}
-		}
-		i++;
-	}
-	return (0);
-}
-
-static int		is_valid_var(char *input)
+char	**tokenize(char *input)
 {
 	int		i;
 	int		j;
-	char	*check;
+	int		k;
 	char	*tmp;
+	char	**ret;
 
-	i = 0;
+	k = 0;
 	j = 0;
+	i = 0;
+	while(input[i] && ft_is_whitespace(input[i]))
+		i++;
 	while(input[i])
 	{
-		printf("%d\n", i);
-		if (input[i] == '=' && input[i + 1])
-		{
-			i--;
-			if (ft_is_whitespace(input[i]))
-			{
-				while(input[i] && ft_is_whitespace(input[i]))
-					i--;
-				while(input[i] && !ft_is_whitespace(input[i]))
-					i--;
-				while(!ft_is_whitespace(input[i]) && input[i])
-				{
-					tmp[j] = input[i];
-					i++;
-					j++;
-				}
-				return (printf("%s: command not found\n", tmp), 1);
-			}
-		}
+		if (input[i] == ' ')
+		tmp[j] = input[i];
 		i++;
+		j++;
 	}
-	return (0);
 }
 
 void	display(void)
@@ -178,20 +95,14 @@ void	display(void)
 	{
 		input = readline("waiting for input:");
 		add_permanent_history(input);
-		add_history("ji");
-		rl_on_new_line();
+		add_history(input);
+		// rl_redisplay();
 		if (!ft_strncmp(input, "exit", 5))
 			break ;
 		if (setup_signals() >= 2)
 			continue ;
-		// if (possible_var(input))
-		// {
-		// 	if (check_var(input))
-		// 		printf("is variable\n");
-		// }
-		is_valid_var(input);
-		// token_stack = ft_tokenize(input);
-		// print_two_d(token_stack);
+		token_stack = tokenize(input);
+		print_two_d(token_stack);
 		free(input);
 	}
 	rl_clear_history();
