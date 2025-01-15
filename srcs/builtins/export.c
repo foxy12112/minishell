3b59af57b4944283317457ff4419170c01e2cae3
Leon Dick <ldick@student.42heilbronn.de>
2025-01-15 16:07:34 +0100

    no more submodules, im not dealing with taht

--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -6,37 +6,11 @@
 /*   By: ldick <ldick@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 02:19:17 by auplisas          #+#    #+#             */
-/*   Updated: 2025/01/15 15:43:19 by ldick            ###   ########.fr       */
+/*   Updated: 2025/01/15 15:57:43 by ldick            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-int	export_variables(t_shell_data *shell, char **variables)
-{
-	int		i;
-	char	**key_value;
-
-	i = 1;
-	while (variables[i])
-	{
-		key_value = ft_split_by_first_equal(variables[i]);
-		if(!valid_key(key_value[0]))
-		{
-			ft_putstr_fd("minishell: export: `", STDERR_FILENO);
-			ft_putstr_fd(key_value[0], STDERR_FILENO);
-			ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
-			shell->last_exit_code = 1;
-			return (shell->last_exit_code);
-		}
-		add_to_variables_list(&shell->env, key_value);
-		add_to_variables_list(&shell->variables, key_value);
-		free_string_array(key_value);
-		i++;
-	}
-	shell->last_exit_code = 0;
-	return (shell->last_exit_code);
-}
 
 void	swap_nodes(t_env_list *a, t_env_list *b)
 {
@@ -148,8 +122,7 @@ int	ft_export(t_shell_data *shell, char **variables)
 	}
 	else
 	{
-		// exit_code = add_variable_ex(shell, variables);
-		exit_code = export_variables(shell, variables);
+		exit_code = add_variable_ex(shell, variables);
 		sort_env_list(shell->variables);
 	}
 	return (exit_code);
